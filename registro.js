a5a5cc731371f22aedd0d67f9d6612f3883ec9d3
Sofia94 <sofiarchi94@gmail.com>
2021-09-30 20:41:07 -0500

    funcion correo

--- registro.js
+++ registro.js
@@ -1,13 +1,58 @@
-function agregarRegistro()
-        {
-        if (typeof arreglocompuesto === 'undefined')
-        {
+    const RegisterForm = document.querySelector('#form-registro');
+    const formdata= [];
+    var correos_filtrados = [];
+    let counter =0;
+
+    RegisterForm.addEventListener('submit',agregarRegistro);
+
+
+function agregarRegistro(){
+        if (typeof arreglocompuesto === 'undefined'){
             globalThis.arreglocompuesto = new Array();
         }
         let valorNombre = document.getElementById("nombre");
+        let valorCorreo = document.getElementById("correo").value;
         let arreglo = [document.getElementById("nombre").value, document.getElementById("apellido").value,document.getElementById("telefono").value,document.getElementById("correo").value,document.getElementById("password").value];
-        arreglocompuesto.push(arreglo);
         
-        alert(arreglocompuesto);
+        formdata.push(arreglo);
+
+       
+        let dataObject = {
+            'correo':       valorCorreo,
+        };
+   
+     
+        RegisterForm.reset();
+        globalThis.arreglocompuesto.push(arreglo);
+        console.log(arreglocompuesto);
+       
+        filtrarCorreo(dataObject);
+
         }
-        module.exports = agregarRegistro;
+
+
+//funcion filtrar correo
+function filtrarCorreo(arreglo) {
+    
+    let filtro = "gmail.com";
+    
+    let mail = arreglo["correo"];
+    
+ 
+    
+    if (mail.includes(filtro)) {
+        correos_filtrados.push(mail);
+        alert(correos_filtrados);
+       
+    }
+
+    console.log(correos_filtrados);
+
+}
+
+//export
+
+module.exports = {
+    agregarRegistro,
+    filtrarCorreo
+}
